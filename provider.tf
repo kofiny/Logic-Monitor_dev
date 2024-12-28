ff2d14ab08f496be07e2561ed68143870a6b2da3
Kofi Nyamekye <kofi_ky@hotmail.com>
2024-12-28 18:37:28 +0000

    remove provider

--- provider.tf
+++ provider.tf
@@ -7,20 +7,18 @@ terraform {
   }
 }
 
+variable "TERRAFORM_AZURE_SECRET_ID" {
+  type = string
+}
+
 provider "azurerm" {
   subscription_id = "78b86be0-6f19-4552-88d4-f12345bea95d"
   tenant_id       = "bcf7e359-c4cc-4f7a-8590-b9c3ea27e2f3"
   client_id       = "125381ce-2451-435d-ab57-7e781754304f"
-  client_secret   = "pW68Q~MVoDnWd2zdmqXq-iv2AhPJjWdcRlexMcCM"
+  client_secret   = var.TERRAFORM_AZURE_SECRET_ID
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
   }
-}
-
-provider "azuread" {
-   tenant_id       = "bcf7e359-c4cc-4f7a-8590-b9c3ea27e2f3"
-  client_id       = "125381ce-2451-435d-ab57-7e781754304f"
-  client_secret   = ".Dv8Q~Sn6Yi9fY4uSVCkDdWUqS3aYRtVGBlsabuf"
 }
